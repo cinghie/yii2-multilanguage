4725b1753cbbaf80383749162b03baeaa0ca9d68
Giandomenico Olini <giando.olini@gogodigital.it>
2015-01-24 21:13:48 +0100

    Update AdvancedUrlManager.php

--- components/AdvancedUrlManager.php
+++ components/AdvancedUrlManager.php
@@ -8,12 +8,12 @@ class AdvancedUrlManager extends UrlManager
 {
     public function createUrl($params)
     {
-        if (!isset($params['language'])) {
-            if (Yii::$app->session->has('language'))
-                Yii::$app->language = Yii::$app->session->get('language');
-            else if(isset(Yii::$app->request->cookies['language']))
-                Yii::$app->language = Yii::$app->request->cookies['language']->value;
-            $params['language']=Yii::$app->language;
+        if (!isset($params['lang'])) {
+            if (Yii::$app->session->has('lang'))
+                Yii::$app->language = Yii::$app->session->get('lang');
+            else if(isset(Yii::$app->request->cookies['lang']))
+                Yii::$app->language = Yii::$app->request->cookies['lang']->value;
+            $params['lang']=Yii::$app->language;
         }
         return parent::createUrl($params);
     }
