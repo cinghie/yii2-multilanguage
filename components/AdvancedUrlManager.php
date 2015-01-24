64bd49a4c660de322bfdbc1a9a4c2a6c194b5b8b
Giandomenico Olini <giando.olini@gogodigital.it>
2015-01-24 15:10:05 +0100

    Create AdvancedUrlManager.php

---
+++ components/AdvancedUrlManager.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace cinghie\yii2-multilinguage\components;
+use yii\web\UrlManager;
+use Yii;
+
+class AdvancedUrlManager extends UrlManager
+{
+    public function createUrl($params)
+    {
+        if (!isset($params['language'])) {
+            if (Yii::$app->session->has('language'))
+                Yii::$app->language = Yii::$app->session->get('language');
+            else if(isset(Yii::$app->request->cookies['language']))
+                Yii::$app->language = Yii::$app->request->cookies['language']->value;
+            $params['language']=Yii::$app->language;
+        }
+        return parent::createUrl($params);
+    }
+}
