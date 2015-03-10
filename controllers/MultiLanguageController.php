a576c9c639ab7e1e5f04efca2adeac1db4b672cd
Giandomenico Olini <giando.olini@gogodigital.it>
2015-03-10 21:46:42 +0100

    Update MultiLanguageController.php

--- controllers/MultiLanguageController.php
+++ controllers/MultiLanguageController.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+* @copyright Copyright &copy; Gogodigital Srls, gogodigital.it, 2015
+* @package yii2-multilanguage
+* @license GNU GPLv3
+* @version 1.0
+*/
+
 namespace cinghie\multilanguage\controllers;
 
 use Yii;
